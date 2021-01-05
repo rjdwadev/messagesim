0a69934d98a7859f1cee8e159968b7d58fd814eb
rjdwadev <rjdwa.dev@gmail.com>
2021-01-04 20:50:52 -0800

    added chat widget

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { API } from 'aws-amplify';
+import { Auth } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listMessages } from './graphql/queries';
+import { createMessage as createMessageMutation, deleteMessage as deleteMessageMutation } from './graphql/mutations';
+import { Widget } from 'react-chat-widget';
+
+import 'react-chat-widget/lib/styles.css';
+
+const initialFormState = { name: '', description: '' }
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+  const [username, setUserName] = '';
+  
+
+  useEffect(() => {
+    fetchMessages();
+  }, []);
+
+  async function fetchMessages() {
+    const apiData = await API.graphql({ query: listMessages });
+    const authdata = await Auth.currentUserInfo();
+    setUserName(authdata.data);
+
+    setMessages(apiData.data.listMessages.items);
+  }
+
+  async function createMessage() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createMessageMutation, variables: { input: formData } });
+    setMessages([ ...messages, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteMessage({ id }) {
+    const newMessageArray = messages.filter(note => note.id !== id);
+    setMessages(newMessageArray);
+    await API.graphql({ query: deleteMessageMutation, variables: { input: { id } }});
+  }
+
   return (
     <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
-      <AmplifySignOut />
+      <div className="App">
+      <Widget />
+    </div>
     </div>
   );
 }
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateMessage = /* GraphQL */ `
+  subscription OnCreateMessage {
+    onCreateMessage {
+      id
+      name
+      description
+      messagefor
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateMessage = /* GraphQL */ `
+  subscription OnUpdateMessage {
+    onUpdateMessage {
+      id
+      name
+      description
+      messagefor
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteMessage = /* GraphQL */ `
+  subscription OnDeleteMessage {
+    onDeleteMessage {
+      id
+      name
+      description
+      messagefor
+      createdAt
+      updatedAt
+    }
+  }
+`;
